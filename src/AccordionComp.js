8fa64119f813b966ac310b4c0ee4eb69bb95816a
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-09-01 16:51:11 +0530

    flex added

--- src/AccordionComp.js
+++ src/AccordionComp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Accordion from 'react-bootstrap/Accordion'
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import './AccordionComp.css';
 
 const AccordionComp = () => {
 		
@@ -9,10 +10,12 @@ const AccordionComp = () => {
 			<Accordion defaultActiveKey="0">
 			  <Card>
 			    <Card.Header>
-			    <p>Showing Result For</p>
-			      <Accordion.Toggle as={Button} variant="link" eventKey="0">
-			        Showing Result For
-			      </Accordion.Toggle>
+			    <div className="flex-div">
+				    <span>Showing Result For</span>
+				      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+				        Showing Result For
+				      </Accordion.Toggle>
+			    </div> 
 			    </Card.Header>
 			    <Accordion.Collapse eventKey="0">
 			      <Card.Body>Hello! I'm the body</Card.Body>
