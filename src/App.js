a7288fa28657c3b94dbe143ea73844cc66f67245
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-09-01 16:36:01 +0530

    accordion added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
-import example from './exampleFunction';
+import AccordionComp from './AccordionComp';
+
 
 class App extends Component{
-	constructor(){
-	    super();
-	    this.state={
-	      open: false
-	    }
-	  }
-
-	setOpen = (x) => {
-		this.setState({open: x})
-	}	  
-
   render(){
     return (
-    	<example open={this.state.open} setOpen={this.setOpen} />
+    	<AccordionComp />
     );    
   }
-
 }
 
 export default App;
