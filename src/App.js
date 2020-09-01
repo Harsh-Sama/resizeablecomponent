eb86967592a0fc0ef6cc44152432f64fbea2519d
Harsh-Sama <harshsharma919@gmail.com>
2020-09-01 15:58:21 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Collapse from 'react-bootstrap/Collapse'
+import { Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+	example = () => {
+		const [open, setOpen] = React.useState(false);
+
+		return(
+			<div>
+			
+			<Button
+		        onClick={() => setOpen(!open)}
+		        aria-controls="example-collapse-text"
+		        aria-expanded={open}
+		      >
+		        click
+		      </Button>
+
+		      <Collapse in={open}>
+		        <div id="example-collapse-text">
+		          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+		          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+		          labore wes anderson cred nesciunt sapiente ea proident.
+		        </div>
+		      </Collapse>
+		      </div>
+			)
+	}
+
+
+  render(){
+    return (
+    	//<example />
+    	<h1>Hello</h1>
+    );    
+  }
+
 }
 
 export default App;
